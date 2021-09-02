c87b10610f0c38ceea563c22e6ab67402646661e
Mustafa <mustafa.adiguzel@std.izmirekonomi.edu.tr>
2021-09-02 12:28:43 +0300

    requests with axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,15 @@
 import { ICoin } from "../types/ICoin";
+import axios from "axios";
 
-export const getAllCoins = async (): Promise<ICoin[]> =>
-  await fetch(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false`
-  )
-  .then(val => val.json()
-);
+enum endpoint {
+  allCoins = '/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false'
+}
+
+export const getAllCoins = async (): Promise<ICoin[]> => {
+    return await 
+    axios.get(endpoint.allCoins)
+      .then(res => res.data);
+}
 
 export const getPage = async (page: number): Promise<ICoin[]> =>
   await fetch(
